7b14e5ab814c3ed48fd1e05b84480fe1a94fd7b0
Lubna Febin <lubnafebin13@gmail.com>
2024-11-19 19:08:51 +0530

    updated with custom changes

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDu7Mv84lkLYrRWPpQ12WyKaVLFq-wuZck",
+    authDomain: "olx-clone-c2347.firebaseapp.com",
+    projectId: "olx-clone-c2347",
+    storageBucket: "olx-clone-c2347.firebasestorage.app",
+    messagingSenderId: "48041550587",
+    appId: "1:48041550587:web:dbe4a47cc9eb7a7366065e",
+    measurementId: "G-4VFV2H45J4"
+};
+export default firebase.initializeApp(firebaseConfig)       
